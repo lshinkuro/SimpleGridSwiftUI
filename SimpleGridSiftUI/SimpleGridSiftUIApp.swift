d8302a7eac5c5f854f4ad1e6792a42d980719f41
nurkholis <nur.kholis@ist.id>
2022-12-17 12:52:34 +0700

    Initial Commit

---
+++ SimpleGridSiftUI/SimpleGridSiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleGridSiftUIApp.swift
+//  SimpleGridSiftUI
+//
+//  Created by nur kholis on 17/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleGridSiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
