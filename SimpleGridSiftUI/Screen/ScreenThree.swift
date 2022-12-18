ddf8e6a1161b1844e4ea9abfad451817dba7e586
nurkholis <nur.kholis@ist.id>
2022-12-18 12:56:26 +0700

    add: stepper in screen 3

--- SimpleGridSiftUI/Screen/ScreenThree.swift
+++ SimpleGridSiftUI/Screen/ScreenThree.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct ScreenThree: View {
+    
+    @State private var value = 0
+    
+    let colors: [Color] = [.orange, .red, .gray, .blue,                               .green, .purple, .pink]
+    
+    func incrementStep() {
+          value += 1
+          if value >= colors.count { value = 0 }
+      }
+
+    func decrementStep() {
+          value -= 1
+          if value < 0 { value = colors.count - 1 }
+      }
+    
     var body: some View {
-        Text("Screen Three")
+        VStack {
+            Stepper {
+                Text("Value: \(value) Color: \(colors[value].description)")
+            } onIncrement: {
+                incrementStep()
+            } onDecrement: {
+                decrementStep()
+            }
+            .padding(5)
+            .background(colors[value])
+            
+            Text("Screen Three")
+        }.padding(10)
     }
 }
 
